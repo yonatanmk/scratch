60bde8de1aede857235abfa8498162ed48c3fea9
Yonatan MK <yonatanmk@gmail.com>
2016-09-01 15:07:19 -0400

    created hangman project

--- hangman.rb
+++ hangman.rb
@@ -96,3 +96,118 @@ word_list = [
 'practical', 'pride', 'promised', 'recall', 'relationship', 'remarkable', 'require', 'rhyme', 'rocky', 'rubbed', 'rush', 'sale', 'satellites', 'satisfied', 'scared', 'selection',
 'shake', 'shaking', 'shallow', 'shout', 'silly', 'simplest', 'slight', 'slip', 'slope', 'soap', 'solar', 'species', 'spin', 'stiff', 'swung', 'tales', 'thumb', 'tobacco', 'toy', 'trap'
 ]
+#----------------------------------------------------------------------------------------------------------------------------
+def random_word word_list
+  return word_list[rand(0..87)]
+end
+
+def draw_board hangman_pics, missed_letters, word_letters, hidden_word
+  puts hangman_pics[missed_letters.length]
+  puts
+  blank = "_" * hidden_word.length
+  char_pos = 0
+  hidden_word.each_char do |x|
+    if word_letters.include? x
+      blank = blank[0...char_pos] + x + blank[char_pos + 1...hidden_word.length]
+    end
+    char_pos += 1
+  end
+  blank.each_char do |x|
+    print x + " "
+  end
+  puts
+  print "Missed Letters: "
+  missed_letters.chars.sort.join.each_char do |x|
+    print x + " "
+  end
+  puts
+end
+
+def get_guess word_letters, missed_letters
+  puts "Please guess a letter."
+  while true
+    guess = gets.chomp.downcase
+    puts
+    if guess.length != 1
+      puts "Please enter only a single letter."
+    elsif word_letters.include? guess or missed_letters.include? guess
+      puts "You have already guessed that letter."
+    elsif not "abcdefghijklmnopqrstuvwxyz".include? guess
+      puts "Please enter a letter. No numbers or symbols will be accepted."
+    else
+      return guess
+    end
+  end
+end
+
+def replay
+  while true
+    puts "Do you want to play again? (yes or no)"
+    play_again_answer = gets.chomp.downcase
+    puts
+    if play_again_answer == "y" || play_again_answer == "yes"
+      puts "Starting a new game..."
+      return true
+    elsif play_again_answer == "n" || play_again_answer == "no"
+      puts "Ending the game..."
+      return false
+    else
+      puts "Please only enter yes or no."
+    end
+  end
+end
+#----------------------------------------------------------------------------------------------------------------------------
+hidden_word = random_word word_list
+missed_letters = ""
+word_letters = ""
+game_over = false
+win_count = 0
+lose_count = 0
+
+puts "Welcome to Hangman!!!"
+puts
+while true
+  draw_board hangman_pics, missed_letters, word_letters, hidden_word
+  guess = get_guess word_letters, missed_letters
+
+  if hidden_word.include? guess
+    word_letters << guess
+    complete_word = true
+    hidden_word.each_char do |x|
+      if not word_letters.include? x
+        complete_word = false
+      end
+    end
+    if complete_word
+      game_over = true
+      win_count += 1
+      puts "Congratulations! You've won! The hidden word was #{hidden_word}!"
+    end
+
+  else
+    missed_letters << guess
+    #puts                                                       #EDIT LATERRRRRRRRRRRR
+    puts "That letter was not in the hidden word."
+    if missed_letters.length == hangman_pics.length - 1
+      puts "G A M E O V E R"
+      puts "You have run out of guesses!"
+      puts "You had #{missed_letters.length} wrong guess#{"es" unless missed_letters.length == 1} and #{word_letters.length} correct guess#{"es" unless word_letters.length == 1}."
+      puts "The word was #{hidden_word}."
+      game_over = true
+      lose_count += 1
+    end
+  end
+
+  if game_over
+    puts "Wins: #{win_count}"
+    puts "Losses: #{lose_count}"
+    if replay
+      hidden_word = random_word word_list
+      missed_letters = ""
+      word_letters = ""
+      game_over = false
+    else
+      break
+    end
+  end
+end
--- misc.rb
+++ misc.rb
@@ -1,9 +1,23 @@
-def listmaker(word_list_file, listnum): # compiles word collection from word_list_file of length 'listnum' into a list
-    word_list_2 = []
-    while True:
-        word_addition = word_list_file.readline()
-        word_addition = word_addition[:len(word_addition) - 1]
-        word_list_2.append(word_addition)
-        if len(word_list_2) == listnum:
-            break
-    return word_list_2
+def get_guess word_letters, missed_letters
+  puts "Please guess a letter."
+  while true
+    guess = gets.chomp.downcase
+    if guess.length != 1
+      puts "Please enter only a single letter."
+    elsif word_letters.include? guess or missed_letters.include? guess
+      puts "You have already guessed that letter."
+    elsif not "abcdefghijklmnopqrstuvwxyz".include? guess
+      puts "Please enter a letter. No numbers or symbols will be accepted."
+    else
+      puts
+      return guess
+    end
+  end
+end
+
+word_letters = "as"
+missed_letters = "zxy"
+
+get_guess word_letters, missed_letters
+
+puts "sentence after"
