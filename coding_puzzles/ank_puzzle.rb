3f6380fc529dfed2aa977032e9537e253bac5c73
Yonatan MK <yonatanmk@gmail.com>
2017-03-08 18:06:47 -0500

    lies, this is the final commit

--- coding_puzzles/ank_puzzle.rb
+++ coding_puzzles/ank_puzzle.rb
@@ -12,12 +12,12 @@ def sentence_composer(input, dictionary, current_sentence = [], current_structur
     dictionary.each do |word_type, words|
       words.each do |word|
         if input.start_with?(word)
-          output << sentence_composer(input.slice(word.size..-1), dictionary, current_sentence << word, current_structure << word_type)
+          output << sentence_composer(input.slice(word.size..-1), dictionary, [*current_sentence, word], [*current_structure, word_type])
         end
       end
     end
   end
-  return output.flatten
+  return output.flatten.reject{|x| x.nil?}
 end
 
 def valid_sentence?(current_structure)
@@ -32,7 +32,7 @@ dictionary = {
   article: ["a", "ac", "e"]
 }
 print sentence_composer("abcdefg", dictionary)
-# puts '_______'
-# print sentence_composer("abcc", dictionary)
-# puts '_______'
-# print sentence_composer("abcd", dictionary)
+puts
+print sentence_composer("abcc", dictionary)
+puts
+print sentence_composer("abcd", dictionary)
