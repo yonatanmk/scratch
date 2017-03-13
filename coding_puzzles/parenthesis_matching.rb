cbcc30eeaff736db3bc525eebc676631ed3bda54
Yonatan MK <yonatanmk@gmail.com>
2017-03-13 14:55:13 -0400

    set up O1 for parens puzz

--- coding_puzzles/parenthesis_matching.rb
+++ coding_puzzles/parenthesis_matching.rb
@@ -7,7 +7,8 @@
 # Example: if the example string above is input with the number 10 (position of the first parenthesis), the output
 # should be 79 (position of the last parenthesis).
 
-def parens_match(input, index)
+
+def parens_matchON(input, index)
   parens = []
   pairs = []
   input.split("").each_with_index do |char, i|
@@ -16,12 +17,27 @@ def parens_match(input, index)
     elsif char == ")"
       pairs.push([parens.pop(), i])
     end
+    puts parens
   end
   puts pairs.find{|pair| pair[0] == index }[1]
 end
 
+# def parens_matchO1(input, index)
+#   parens = 0
+#   pairs = []
+#   input.split("").each_with_index do |char, i|
+#     if char == "("
+#       parens += 1
+#     elsif char == ")"
+#       pairs.push([parens.pop(), i])
+#     end
+#   end
+#   puts pairs.find{|pair| pair[0] == index }[1]
+# end
+
 input = "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
-parens_match(input, 28)
+parens_matchON(input, 28)
+# parens_matchO1(input, 28)
 
 # A stack is like a stack of plates. It's "last in, first out" (LIFO), which means the item that was put in the stack most recently is the first item that comes out.
 #
